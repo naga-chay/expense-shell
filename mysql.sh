a2bfc40f850a7f42e718627796cb843e4a3af866
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-13 23:52:25 +0530

    sqlscript

--- mysql.sh
+++ mysql.sh
@@ -44,11 +44,11 @@ VALIDATE $? "Starting MySQL Server"
 
 #Below code will be useful for idempotent nature
 
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h dbsql.naga.website -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-   VALIDATE $? "MySQL Root password Setup"
+  VALIDATE $? "MySQL Root password Setup"
 else
   echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
